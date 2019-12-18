d09a4cbfaab17fd3d90f15c769f9babb1d250066
brickatyourfeet <brickatyourfeet@gmail.com>
2019-12-17 19:49:15 -0800

    Initial Commit

---
+++ ChatChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatChat
+//
+//  Created by Kyle Braden on 12/17/19.
+//  Copyright © 2019 Kyle Braden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
